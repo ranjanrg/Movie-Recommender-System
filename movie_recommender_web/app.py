22e9b1418aa23bc336001f884a84791973680212
Ranjan <ranjanrg.dev@gmail.com>
2026-04-23 12:12:01 +0530

    Refactor app structure and enhance UI for movie recommendations

--- movie_recommender_web/app.py
+++ movie_recommender_web/app.py
@@ -1,10 +1,17 @@
+from pathlib import Path
+
 from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
 
-movies = pickle.load(open('movies.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+BASE_DIR = Path(__file__).resolve().parent
+
+with open(BASE_DIR / 'movies.pkl', 'rb') as movies_file:
+    movies = pickle.load(movies_file)
+
+with open(BASE_DIR / 'similarity.pkl', 'rb') as similarity_file:
+    similarity = pickle.load(similarity_file)
 
 
 def recommend(movie):
@@ -28,17 +35,20 @@ def recommend(movie):
 @app.route('/', methods=['GET', 'POST'])
 def home():
     recommendations = []
+    selected_movie = ''
 
     if request.method == 'POST':
-        selected_movie = request.form['movie']
-        recommendations = recommend(selected_movie)
+        selected_movie = request.form.get('movie', '')
+        if selected_movie:
+            recommendations = recommend(selected_movie)
 
     movie_list = movies['title'].values
 
     return render_template(
         'index.html',
         movie_list=movie_list,
-        recommendations=recommendations
+        recommendations=recommendations,
+        selected_movie=selected_movie
     )
 
 
